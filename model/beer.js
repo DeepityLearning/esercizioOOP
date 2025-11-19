d56ccc4405199b539df04bea391abed36fdbe0c5
labstraction <labstraction@protonmail.com>
2025-11-19 13:26:10 +0100

    start

---
+++ model/beer.js
@@ -0,0 +1,10 @@
+class Beer extends Beverage{
+
+    constructor(name, producer, type, color){
+        super(name, producer);
+        this.type = type;
+        this.color = color;
+    }
+
+
+}
---
+++ model/beverage.js
@@ -0,0 +1,7 @@
+class Beverage{
+
+    constructor(name, producer){
+        this.name = name;
+        this.producer = producer;
+    }
+}
